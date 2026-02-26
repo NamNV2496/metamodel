aa601e4b6ac2fb8f072c52cf870e2ae508ea83ba
namnv2496 <namnv2496@gmail.com>
2026-02-27 01:46:32 +0700

    update flag description

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ var (
 	destination = flag.String("destination", "", "Output file for generated code (default: <source>_metamodel.go, e.g., models_metamodel.go)")
 	packageName = flag.String("packageName", "metamodel", "Package name for generated file (default: same as source, e.g., models)")
 	tag         = flag.String("tag", "json", "Specific tag name to generate (optional, e.g., json, bson, gorm)")
-	tableName   = flag.String("tableName", "", "Specific table name to generate (optional, e.g., json, bson, gorm)")
+	tableName   = flag.String("tableName", "", "Specific table name to generate (default: <structName>s, optional for custome e.g., users, mock_test)")
 )
 
 func main() {
@@ -22,6 +22,11 @@ func main() {
 
 	if *source == "" {
 		fmt.Fprintln(os.Stderr, "Error: -source flag is required")
+		flag.Usage()
+		os.Exit(1)
+	}
+	if *destination == "" {
+		fmt.Fprintln(os.Stderr, "Error: -destination flag is required")
 		flag.Usage()
 		os.Exit(1)
 	}
